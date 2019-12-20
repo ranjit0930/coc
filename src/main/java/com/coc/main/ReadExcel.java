a3838a659a9d64d77d0decbdf0bf182117356399
Ranjit Singh <ranjit.093076@gmail.com>
2019-12-20 10:15:49 +0530

    Changes

--- src/main/java/com/coc/main/ReadExcel.java
+++ src/main/java/com/coc/main/ReadExcel.java
@@ -27,12 +27,66 @@ public class ReadExcel {
 //creating workbook instance that refers to .xls file
         XSSFWorkbook wb = new XSSFWorkbook(fis);
 //creating a Sheet object to retrieve the object
-        XSSFSheet sheet = wb.getSheetAt(2);
-        System.out.println(sheet.getSheetName());
+        XSSFSheet sheetIndia = wb.getSheetAt(2);
+        XSSFSheet sheetSA = wb.getSheetAt(4);
+        calculateTotalRun(sheetIndia,wb);
+        calculateTotalRun(sheetSA,wb);
+        //System.out.println(sheet.getSheetName());
 //evaluating cell type
+//        FormulaEvaluator formulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        List<HashMap<String,String>> listOfBowlingDetails=new ArrayList<>();
+//        Double totalRuns=0.0;
+//        Double totalWicket=0.0;
+//        for (Row row : sheet)     //iteration over row using for each loop
+//        {
+//            // BowlingRecords bowlingRecords=new BowlingRecords();
+//            HashMap<String,String> hmap=new HashMap<>();
+//            for (Cell cell : row)    //iteration over cell using for each loop
+//            {
+//                if (row.getRowNum() == 0) {
+//                    arrayList.add(cell.getStringCellValue());
+//                } else if (cell.getColumnIndex() == 0 && !bowlingSets.containsKey(cell.getStringCellValue())) {
+//                    ArrayList<HashMap<String,String>> arrayList1=new ArrayList<>();
+//                    bowlingSets.put(cell.getStringCellValue(), arrayList1);
+//                }
+//                  else if(cell.getColumnIndex() == 0){
+//                       //doNothing
+//                    }
+//                 else {
+//                    switch (formulaEvaluator.evaluateInCell(cell).getCellType()) {
+//                        case Cell.CELL_TYPE_NUMERIC:   //field that represents numeric cell type
+//                            hmap.put(arrayList.get(cell.getColumnIndex()), String.valueOf(cell.getNumericCellValue()));
+//                            //bowlingSets.replace(row.getCell(0).getStringCellValue(), hmap);
+//                            if(arrayList.get(cell.getColumnIndex()).equals("Runs_Scored")){
+//                                totalRuns=totalRuns+cell.getNumericCellValue();
+//                            }
+//                            if(arrayList.get(cell.getColumnIndex()).equals("Wicket_Taken")){
+//                                totalWicket=totalWicket+cell.getNumericCellValue();
+//                            }
+//                            break;
+//                        case Cell.CELL_TYPE_STRING:    //field that represents string cell type
+//                            hmap.put(arrayList.get(cell.getColumnIndex()), cell.getStringCellValue());
+//                            //bowlingSets.replace(row.getCell(0).getStringCellValue(), hmap);
+//                            break;
+//                    }
+//                }
+//            }
+//
+//            if(!bowlingSets.isEmpty()){
+//             bowlingSets.get(row.getCell(0).getStringCellValue()).add(hmap);
+//            }
+//        }
+//        System.out.println(bowlingSets);
+//        System.out.println(totalRuns +"_"+totalWicket);
+    }
+    public static void calculateTotalRun(XSSFSheet sheet,XSSFWorkbook wb){
+        HashMap<String, List<HashMap<String, String>>> bowlingSets = new HashMap<>();
         FormulaEvaluator formulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
         ArrayList<String> arrayList = new ArrayList<>();
         List<HashMap<String,String>> listOfBowlingDetails=new ArrayList<>();
+        Double totalRuns=0.0;
+        Double totalWicket=0.0;
         for (Row row : sheet)     //iteration over row using for each loop
         {
             // BowlingRecords bowlingRecords=new BowlingRecords();
@@ -45,16 +99,20 @@ public class ReadExcel {
                     ArrayList<HashMap<String,String>> arrayList1=new ArrayList<>();
                     bowlingSets.put(cell.getStringCellValue(), arrayList1);
                 }
-                  else if(cell.getColumnIndex() == 0){
-                       //doNothing
-                    }
-                 else {
-                    //HashMap<String, String>hmap = bowlingSets.get(row.getCell(0).getStringCellValue());
-
+                else if(cell.getColumnIndex() == 0){
+                    //doNothing
+                }
+                else {
                     switch (formulaEvaluator.evaluateInCell(cell).getCellType()) {
                         case Cell.CELL_TYPE_NUMERIC:   //field that represents numeric cell type
                             hmap.put(arrayList.get(cell.getColumnIndex()), String.valueOf(cell.getNumericCellValue()));
                             //bowlingSets.replace(row.getCell(0).getStringCellValue(), hmap);
+                            if(arrayList.get(cell.getColumnIndex()).equals("Runs_Scored")){
+                                totalRuns=totalRuns+cell.getNumericCellValue();
+                            }
+                            if(arrayList.get(cell.getColumnIndex()).equals("Wicket_Taken")){
+                                totalWicket=totalWicket+cell.getNumericCellValue();
+                            }
                             break;
                         case Cell.CELL_TYPE_STRING:    //field that represents string cell type
                             hmap.put(arrayList.get(cell.getColumnIndex()), cell.getStringCellValue());
@@ -63,10 +121,12 @@ public class ReadExcel {
                     }
                 }
             }
-            if(bowlingSets.containsKey(row.getCell(0))){
-             bowlingSets.get(row.getCell(0)).add(hmap);
+
+            if(!bowlingSets.isEmpty()){
+                bowlingSets.get(row.getCell(0).getStringCellValue()).add(hmap);
             }
         }
         System.out.println(bowlingSets);
+        System.out.println(totalRuns +"_"+totalWicket);
     }
 }
